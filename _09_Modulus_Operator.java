4a970c8c8fd57e355344c02e1dc24304665c28b7
Sandesh <sandeshc933@gmail.com>
2026-09-01 19:22:48 +0530

    feat: add Java basics examples for modulus, division, and char data types

---
+++ _09_Modulus_Operator.java
@@ -0,0 +1,11 @@
+public class _09_Modulus_Operator {
+    public static void main(String[] args){
+        System.out.println(11 % 2 + " is the remainder when " + 11 + " is divided by " + 2);
+        System.out.println("");
+        System.out.println("Properties :");
+        System.out.println("");
+        System.out.println("a % b = x (if a < b)");
+        System.out.println("a % (-b) = a % b");
+        System.out.println("(-a) % b = -(a % b)");
+    }
+}
