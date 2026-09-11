6929f9941a89df0367f45a14b1cb480fdd0cd6d1
Sandesh <sandeshc933@gmail.com>
2026-09-11 23:54:19 +0530

    fix: correct class name casing and add composite number check program

--- _03_Loops/_11_break_continue.java
+++ _03_Loops/_11_break_continue.java
@@ -1,6 +1,6 @@
 package _03_Loops;
 
-public class _11_break_continue {
+public class _11_Break_continue {
     public static void main(String[] args){
     //  break - it is used to terminate the loop
     //  continue - it is used to skip a paricular iteration
---
+++ _03_Loops/_12_Question10.java
@@ -0,0 +1,29 @@
+//  WAP to check if a given number is composite or not
+
+package _03_Loops;
+
+import java.util.Scanner;
+
+public class _12_Question10 {
+    public static void main(String[] args){
+          Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+
+        boolean flag = true; 
+
+        for ( int i = 2 ; i < n ; i++){
+            if ( n % i == 0){
+                flag = false;
+                break;
+            }
+        }
+        
+        if (n == 1) System.out.print("Neither Prime nor Composite number");
+        else if (flag == false) System.out.print("Composite number");
+        else System.out.print("Prime number");
+
+        sc.close();
+    }
+}
